7a4dac5dde56762009cf3b38d286e53b8b87e493
Yiqing Zhao <v-yiqz@MININT-SLJ9M3D.fareast.corp.microsoft.com>
2016-02-01 11:19:06 +0800

    Blob Test

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+            var blobclient = storageAccount.CreateCloudBlobClient();
+
+            // 检查container是否被创建，如果没有，创建container
+            var container = blobclient.GetContainerReference("helloworldcontainer");
+            container.CreateIfNotExists();
+
+            // 新建一个blob，内容为"Hello World"
+            var blob = container.GetBlockBlobReference("myfile");
+            /*using (var fileStream = System.IO.File.OpenRead("myfile"))
+            {
+                blob.UploadFromStream(fileStream);
+            }*/
+            blob.UploadText("helloWorld");
+      
+            // 读取并显示blob内容
+            var blobcontent = blob.DownloadText();
+            Console.WriteLine(blobcontent);
+           
+            // 删除blob
+            var succeed = blob.DeleteIfExists();
+            Console.WriteLine(succeed ? "Delete Succeed" : "Delete Failed");
+            Console.ReadLine();
+        }
+    }
+}
